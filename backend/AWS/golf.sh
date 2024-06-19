4440e44f20349b6eb54782452f158f7d1bd7c7ec
Nathaniel Kidder <nrkidder@yahoo.com>
2024-06-19 14:00:26 -0400

    add backend

---
+++ backend/AWS/golf.sh
@@ -0,0 +1,12 @@
+mv aws_02_qa_2.txt aws_02_qa.txt
+mv aws_03_qa_2.txt aws_03_qa.txt
+mv aws_04_qa_2.txt aws_04_qa.txt
+mv aws_05_qa_2.txt aws_05_qa.txt
+mv aws_06_qa_2.txt aws_06_qa.txt
+mv aws_07_qa_2.txt aws_07_qa.txt
+mv aws_08_qa_2.txt aws_08_qa.txt
+mv aws_09_qa_2.txt aws_09_qa.txt
+mv aws_10_qa_2.txt aws_10_qa.txt
+mv aws_11_qa_2.txt aws_11_qa.txt
+mv aws_12_qa_2.txt aws_12_qa.txt
+mv aws_13_qa_2.txt aws_13_qa.txt
